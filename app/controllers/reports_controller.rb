7944b3e8565ee684f9ab7fe427ffc26ea97f34a3
hana-0612 <hyania1206@gmail.com>
2021-11-20 16:20:10 +0900

    報告ページのコントローラー作成

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,32 @@
 class ReportsController < ApplicationController
   def index
   end
+
+  require "date"
+
+  def new
+    @reports = Report.new
+  end
+
+  def create
+    @reports = Report.new(reports_params)
+    case params[:commit]
+      when "出勤" ; @reports.start_of_work = Time.now
+      when "開始" ; @reports.breakstart = Time.now
+      when "終了" ; @reports.breakend = Time.now
+      when "退勤" ; @reports.end_of_work = Time.now
+      when "報告する" ; @reports.business_content
+    end
+    if @reports.save
+      redirect_to root_path
+    else
+      render :index
+    end  
+  end
+
+  private
+  def reports_params
+    params.permit(:start_of_work, :breakstart, :breakend, :end_of_work).merge(user_id: current_user.id)
+  end
+
 end
